398649ea301272d53c88b4679588d16f661bc111
grayzerguy <grayzerg@gamil.com>
2024-09-13 20:58:42 +0300

    northwind_1

---
+++ src/Components/LayoutArea/Footer/Footer.tsx
@@ -0,0 +1,15 @@
+import "./Footer.css"
+
+function Footer(): JSX.Element {
+
+    return (
+        <div className="Footer">
+           <p>All Rights Reserved &copy;</p>
+        </div>
+    )
+
+}
+
+export default Footer
+
+
---
+++ src/Components/LayoutArea/Layout/Layout.tsx
@@ -0,0 +1,21 @@
+import Footer from "../Footer/Footer"
+import Header from "../Header/Haeder"
+import "./Layout.css"
+
+function Layout(): JSX.Element {
+
+    return (
+        <div className="Layout">
+            <header><Header /></header>
+            <aside>
+
+            </aside>
+            <main>3</main>
+            <footer> <Footer /></footer>
+
+        </div>
+    )
+
+}
+
+export default Layout
